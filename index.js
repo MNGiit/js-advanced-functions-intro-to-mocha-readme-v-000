44cff1e632a2192f51a46700492dd87dae91bd2a
Mohammed Noori <kmanoorib@gmail.com>
2019-04-01 04:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ function sayHeyFriend(name) {return 'Hey, ' + name + '!'}
 
 function shouting(word) {return word.toUpperCase()}
 
-function roundDown(num) {return num}
+function roundDown(num) {return num.floor()}
